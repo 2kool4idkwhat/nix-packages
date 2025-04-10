bd626b139bb764c126bd3b70a747e44bb8169836
Luna 2kool4idkwhat <120322313+2kool4idkwhat@users.noreply.github.com>
2025-04-10 13:32:43 +0200

    shorten desktop item names

--- pkgs/mine/open-adw-docs/default.nix
+++ pkgs/mine/open-adw-docs/default.nix
@@ -2,7 +2,7 @@
 
 makeDesktopItem {
   name = "openAdwDocs";
-  desktopName = "Libadwaita Documentation";
+  desktopName = "Libadwaita Docs";
   exec = "${xdg-utils}/bin/xdg-open ${libadwaita.devdoc}/share/doc/libadwaita-1/index.html";
   icon = "${libadwaita.devdoc}/share/icons/hicolor/scalable/apps/org.gnome.Adwaita1.Demo.svg";
 }
--- pkgs/mine/open-gtk4-docs/default.nix
+++ pkgs/mine/open-gtk4-docs/default.nix
@@ -2,7 +2,7 @@
 
 makeDesktopItem {
   name = "openGtk4docs";
-  desktopName = "GTK 4 Documentation";
+  desktopName = "GTK 4 Docs";
   exec = "${xdg-utils}/bin/xdg-open ${gtk4.devdoc}/share/doc/gtk4/index.html";
   icon = "${gtk4}/share/icons/hicolor/scalable/apps/org.gtk.Demo4.svg";
 }
