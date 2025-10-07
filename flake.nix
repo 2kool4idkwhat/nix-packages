95522be2176f33d5a21b4db22fdc7e9e05f7f9cf
Luna 2kool4idkwhat <120322313+2kool4idkwhat@users.noreply.github.com>
2025-10-07 20:00:02 +0200

    use nixpkgs unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My nix packages";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     flake-utils.url = "github:numtide/flake-utils";
   };
