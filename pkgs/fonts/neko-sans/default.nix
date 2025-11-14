950c762dda54330cfb1ea5a70e74a4d3809a8acb
Luna 2kool4idkwhat <120322313+2kool4idkwhat@users.noreply.github.com>
2025-11-14 20:26:49 +0100

    vendor neko sans

--- pkgs/fonts/neko-sans/default.nix
+++ pkgs/fonts/neko-sans/default.nix
@@ -1,36 +1,17 @@
 {
   lib,
   stdenvNoCC,
-  fetchzip,
-  python3Packages,
 }:
 
-stdenvNoCC.mkDerivation rec {
+stdenvNoCC.mkDerivation {
   pname = "neko-sans";
   version = "4.1";
 
-  src = fetchzip {
-    url = "https://github.com/rsms/inter/releases/download/v${version}/Inter-${version}.zip";
-    stripRoot = false;
-    hash = "sha256-5vdKKvHAeZi6igrfpbOdhZlDX2/5+UvzlnCQV6DdqoQ=";
-  };
+  src = ./.;
 
-  nativeBuildInputs = [
-    python3Packages.opentype-feature-freezer
-  ];
-
+  # TODO: find what broke the opentype-feature-freezer patching
   installPhase = ''
     runHook preInstall
-
-    pyftfeatfreeze --features "ss04" --replacenames "Inter Variable/Neko Sans" \
-      "InterVariable.ttf" "NekoSans-Regular.ttf"
-    pyftfeatfreeze --features "ss04" --replacenames "Inter Variable/Neko Sans" \
-      "InterVariable-Italic.ttf" "NekoSans-Italic.ttf"
-
-    pyftfeatfreeze --features "ss02" --replacenames "Inter Variable/Neko Slashed" \
-      "InterVariable.ttf" "NekoSlashed-Regular.ttf"
-    pyftfeatfreeze --features "ss02" --replacenames "Inter Variable/Neko Slashed" \
-      "InterVariable-Italic.ttf" "NekoSlashed-Italic.ttf"
 
     mkdir -p $out/share/fonts/
     cp Neko* $out/share/fonts/
